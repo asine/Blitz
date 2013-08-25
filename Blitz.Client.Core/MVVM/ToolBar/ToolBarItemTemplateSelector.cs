cc1e343d7dd879b6e20cd2629c5121b6f98536b9
Peter Strong <xjpeter@gmail.com>
2013-08-25 18:55:13 +0100

    Added ToolBarItemTemplateSelector.

---
+++ Blitz.Client.Core/MVVM/ToolBar/ToolBarItemTemplateSelector.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Blitz.Client.Core.MVVM.ToolBar
+{
+    public class ToolBarItemTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            DataTemplate template = null;
+
+            var typeName = item.GetType().Name;
+            switch (typeName)
+            {
+                case "ToolBarButtonItem":
+                {
+                    template = Application.Current.TryFindResource("ToolBarButtonItemTemplate") as DataTemplate;
+                    break;
+                }
+            }
+
+            return template;
+        }
+    }
+}
