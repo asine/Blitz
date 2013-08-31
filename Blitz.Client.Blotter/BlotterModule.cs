d6982816e62c01c0f00e7a03f60422ccbc379703
Peter Strong <xjpeter@gmail.com>
2013-08-31 22:10:09 +0100

    Added Blotter module.

--- Blitz.Client.Blotter/BlotterModule.cs
+++ Blitz.Client.Blotter/BlotterModule.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Blitz.Client.Core.MVVM;
+using Blitz.Common.Core;
+
+using Microsoft.Practices.Prism.Modularity;
 
 namespace Blitz.Client.Blotter
 {
-    class BlotterModule
+    public class BlotterModule : IModule
     {
+        private readonly ILog _log;
+        private readonly IViewService _viewService;
+
+        public BlotterModule(ILog log, IViewService viewService)
+        {
+            _log = log;
+            _viewService = viewService;
+        }
+
+        public void Initialize()
+        {
+        }
     }
 }
